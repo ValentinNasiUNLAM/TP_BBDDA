f602a3c29ec9b3521bf874ebea251759c22ba7ef
Kevin Arias <kearias@alumno.unlam.edu.ar>
2025-07-05 02:04:21 -0300

    se corrige BulkInsert de RP y se agrega gitignore

--- Grupo07/Grupo07/BulkInsertResponsablesDePago.sql
+++ Grupo07/Grupo07/BulkInsertResponsablesDePago.sql
@@ -37,52 +37,30 @@ BEGIN
 
 	EXEC sp_executesql @sql, N'@Ruta NVARCHAR(500)', @Ruta = @ruta_archivo;
 
-	-- Inserto prestadores
-	INSERT INTO tabla.PrestadoresSalud (nombre, telefono)
-	SELECT RTRIM(LTRIM(nombre_obra_social)), RTRIM(LTRIM(telefono_obra_social))
-	FROM #ResponsablesTemp r
-	GROUP BY RTRIM(LTRIM(nombre_obra_social)), RTRIM(LTRIM(telefono_obra_social));
-
-	--ALTERNATIVA PRESTADORES SALUD
-
-	/*INSERT INTO tabla.PrestadoresSalud (nombre, telefono)
-	SELECT RTRIM(LTRIM(nombre_obra_social)), RTRIM(LTRIM(telefono_obra_social))
-	FROM #ResponsablesTemp r
-	WHERE NOT EXISTS (
-		SELECT 1 
-		FROM tabla.PrestadoresSalud ps
-		WHERE RTRIM(LTRIM(ps.nombre)) = RTRIM(LTRIM(r.nombre_obra_social))
-	);*/
-
-	WITH ResponsablesSinDNIDuplicado AS(
-		SELECT *, ROW_NUMBER() OVER (PARTITION BY dni ORDER BY (SELECT NULL)) AS rn
-		FROM #ResponsablesTemp
+	-- Insertar Prestadores de Salud
+	WITH PrestadoresSinDuplicados AS (
+	    SELECT 
+	        RTRIM(LTRIM(nombre_obra_social)) AS nombre,
+	        RTRIM(LTRIM(telefono_obra_social)) AS telefono,
+	        ROW_NUMBER() OVER (
+	            PARTITION BY RTRIM(LTRIM(nombre_obra_social))
+	            ORDER BY telefono_obra_social
+	        ) AS rn
+	    FROM #ResponsablesTemp
 	)
-	INSERT INTO tabla.Socios (nro_socio, dni, estado, nombre, apellido, email, fecha_nacimiento, telefono, telefono_emergencia, id_prestador_salud, nro_socio_obra_social)
-	SELECT
-		TRY_CAST(REPLACE(RTRIM(LTRIM(nro_socio)), 'SN-','') as INT) as nrosocio,
-		TRY_CAST(dni AS INT) as dni,
-		1 as estado,
-		RTRIM(LTRIM(rt.nombre)) as nombre,
-		RTRIM(LTRIM(apellido)) as apellido,
-		RTRIM(LTRIM(email)) as correo,
-		TRY_CAST(fecha_nacimiento AS DATE) as fnac,
-		TRY_CAST(telefono_contacto AS INT) as telc,
-		TRY_CAST(rt.telefono_emergencia AS INT) as telem,
-		ps.id_prestador_salud as id_prestador,
-		rt.nro_socio_obra_social as nro_socio_obra_social
-	FROM ResponsablesSinDNIDuplicado rt
-	LEFT JOIN tabla.PrestadoresSalud ps
-		ON LTRIM(RTRIM(ps.nombre)) = LTRIM(RTRIM(rt.nombre_obra_social))
+	INSERT INTO tabla.PrestadoresSalud (nombre, telefono)
+	SELECT nombre, telefono
+	FROM PrestadoresSinDuplicados pssd
 	-- Tomar solamente el primer valor (rn=1) de los dni repetidos
-	WHERE rn = 1 AND NOT EXISTS (
-		SELECT 1 FROM tabla.Socios s 
-		WHERE s.dni = TRY_CAST(rt.dni AS INT)
+	WHERE rn = 1
+	  AND NOT EXISTS (
+	    SELECT 1 
+	    FROM tabla.PrestadoresSalud ps
+	    WHERE RTRIM(LTRIM(ps.nombre)) = pssd.nombre
 	);
 
-	--ALTERNATIVA SOCIOS
-
-	/*WITH ResponsablesSinDNIDuplicado AS (
+	-- Insertar Responsables
+	WITH ResponsablesSinDNIDuplicado AS (
 		SELECT *, ROW_NUMBER() OVER (PARTITION BY dni ORDER BY (SELECT NULL)) AS rn
 		FROM #ResponsablesTemp
 		WHERE ISNUMERIC(dni) = 1 AND RTRIM(LTRIM(dni)) <> ''
@@ -111,8 +89,8 @@ BEGIN
 			WHERE s.dni = TRY_CAST(rt.dni AS INT) 
 			OR s.email = rt.email
 			OR s.nro_socio = rt.nro_socio
-		);*/
+		);
 END;
 
 -- Descomentar para ejecuci�n:
--- EXEC spInsercion.sp_importar_responsables @ruta_archivo=N'C:\Users\kevin\TP_BBDDA\CSV\responsables_pago.csv';
+--EXEC spInsercion.ImportarResponsables @ruta_archivo=N'C:\Users\kevin\TP_BBDDA\CSV\responsables_pago.csv';
