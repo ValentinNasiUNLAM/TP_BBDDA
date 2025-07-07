a77d1220f5a2afcd57d589b1c13813164a2f2e1f
Kevin Arias <kearias@alumno.unlam.edu.ar>
2025-07-07 02:39:25 -0300

    se corrige script de pagos

--- Grupo07/Grupo07/BulkInsertPagos.sql
+++ Grupo07/Grupo07/BulkInsertPagos.sql
@@ -102,7 +102,7 @@ BEGIN
         WHERE p.nro_pago = t.id_pago
     );*/
 
-    WITH PagosValidos AS (
+    /*WITH PagosValidos AS (
        SELECT
         t.id_pago,
         TRY_CAST(REPLACE(RTRIM(LTRIM(t.nro_socio)), 'SN-', '') AS INT) AS nro_socio,
@@ -149,9 +149,56 @@ BEGIN
     WHERE pcf.rn = 1
     AND NOT EXISTS (
       SELECT 1 FROM administracion.Pagos p WHERE p.nro_pago = pcf.id_pago
-    );
+    );*/
+WITH PagosValidos AS (
+    SELECT
+        t.id_pago,
+        TRY_CAST(REPLACE(RTRIM(LTRIM(t.nro_socio)), 'SN-', '') AS INT) AS nro_socio,
+        t.mediopago,
+        TRY_CAST(t.fecha AS DATE) AS fecha,
+        t.Valor
+    FROM #PagosTemp t
+    WHERE NOT EXISTS (
+        SELECT 1 FROM administracion.Pagos p WHERE p.nro_pago = t.id_pago
+    )
+),
+PagosNumerados AS (
+    SELECT *,
+           ROW_NUMBER() OVER (PARTITION BY nro_socio ORDER BY fecha) AS rn
+    FROM PagosValidos
+),
+FacturasNumeradas AS (
+    SELECT
+        f.numero_factura,
+        s.nro_socio,
+        f.fecha_creacion,
+        ROW_NUMBER() OVER (PARTITION BY s.nro_socio ORDER BY f.fecha_creacion) AS rn
+    FROM administracion.FacturasARCA f
+    JOIN socios.Socios s ON f.id_socio = s.id_socio
+    WHERE NOT EXISTS (
+        SELECT 1 FROM administracion.Pagos p WHERE p.numero_factura = f.numero_factura
+    )
+)
+INSERT INTO administracion.Pagos
+    (nro_pago, numero_factura, id_medio_pago, fecha, total, reembolso)
+SELECT
+    TRY_CAST(p.id_pago AS BIGINT),
+    f.numero_factura,
+    administracion.BuscarIDPago(p.mediopago),
+    p.fecha,
+    p.Valor,
+    0
+FROM PagosNumerados p
+JOIN FacturasNumeradas f
+    ON p.nro_socio = f.nro_socio AND p.rn = f.rn;
     
     END;
 
 -- Descomentar para ejecuci�n:
 -- EXEC administracion.ImportarPagos @ruta_archivo_pagos=N'C:\Users\kevin\TP_BBDDA\CSV\pago_cuotas.csv'; 
+
+-- Tabla formato pago_cuotas.csv
+/*SELECT p.nro_pago as IdDePago, p.fecha as Fecha, s.nro_socio as ResponsablePago, p.total as Valor FROM administracion.Pagos p
+INNER JOIN administracion.FacturasARCA f
+ON p.numero_factura = f.numero_factura
+LEFT JOIN socios.Socios s ON s.id_socio = f.id_socio*/
